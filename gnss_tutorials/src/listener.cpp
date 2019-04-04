d62bb245c0e6379dba2bb35c6632e0d81cdde216
HATTORI Kohei <hattori@team-lab.com>
2019-04-05 01:41:18 +0800

    add listener

---
+++ gnss_tutorials/src/listener.cpp
@@ -0,0 +1,31 @@
+#include "ros/ros.h"
+#include "sensor_msgs/NavSatFix.h"
+
+
+void baseFixCallback(const sensor_msgs::NavSatFix::ConstPtr& msg)
+{
+    ROS_INFO("latitude: [%f]", msg->latitude);
+    ROS_INFO("longitude: [%f]", msg->longitude);
+    ROS_INFO("altitude: [%f]", msg->altitude);
+}
+
+
+void roverFixCallback(const sensor_msgs::NavSatFix::ConstPtr& msg)
+{
+    ROS_INFO("latitude: [%f]", msg->latitude);
+    ROS_INFO("longitude: [%f]", msg->longitude);
+    ROS_INFO("altitude: [%f]", msg->altitude);
+}
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "listener");
+    ros::NodeHandle n;
+
+    ros::Subscriber sub = n.subscribe("rover/fix", 100, roverFixCallback);
+
+    ros::spin();
+
+    return 0;
+}
